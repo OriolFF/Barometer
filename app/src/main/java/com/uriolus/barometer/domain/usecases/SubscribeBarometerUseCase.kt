8998a8aa16813815bb8432c71a5b3bb8f7aafabb
Oriol Fernandez <oriol.developer@gmail.com>
2025-06-28 21:26:41 +0100

    DistinctUntiChangedBy to improve performance

--- app/src/main/java/com/uriolus/barometer/domain/usecases/SubscribeBarometerUseCase.kt
+++ app/src/main/java/com/uriolus/barometer/domain/usecases/SubscribeBarometerUseCase.kt
@@ -3,9 +3,14 @@ package com.uriolus.barometer.domain.usecases
 import com.uriolus.barometer.domain.model.BarometerReading
 import com.uriolus.barometer.domain.repository.BarometerRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChangedBy
+import kotlin.math.roundToInt
 
 class SubscribeBarometerUseCase(private val repository: BarometerRepository) {
-    operator fun invoke(): Flow<BarometerReading> {
-        return repository.getBarometerReadings()
-    }
+    operator fun invoke(): Flow<BarometerReading> =
+        repository.getBarometerReadings()
+            .distinctUntilChangedBy {
+                // Compare rounded values to avoid reacting to insignificant fluctuations
+                (it.pressure * 100).roundToInt()
+            }
 }
