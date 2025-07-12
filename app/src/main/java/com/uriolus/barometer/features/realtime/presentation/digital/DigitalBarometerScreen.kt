dc9fdb24fe7d3b4c8bcb2663a1282ac018e96df0
Oriol Fernandez <oriol.developer@gmail.com>
2025-07-12 19:25:38 +0100

    Update AGP vrsion

--- app/src/main/java/com/uriolus/barometer/features/realtime/presentation/digital/DigitalBarometerScreen.kt
+++ app/src/main/java/com/uriolus/barometer/features/realtime/presentation/digital/DigitalBarometerScreen.kt
@@ -2,7 +2,6 @@ package com.uriolus.barometer.features.realtime.presentation.digital
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
